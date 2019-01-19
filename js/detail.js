2534f19c53bf05490bc9679ec13e074c695fb1f0
Akuria <15620607482@163.com>
2019-01-19 19:23:54 +0800

    detail页完成（项目已全部完成 购物车功能未实现）

--- js/detail.js
+++ js/detail.js
@@ -1,17 +1,17 @@
 var loginOrNot = false;
 // setCookie("login", "false")
 loginOrNot = getCookie("login")
-    if (loginOrNot) {
-        document.getElementsByClassName("arr")[0].style.position = "relative"
-        document.getElementsByClassName("arr")[0].style.top = "-12px"
-        document.getElementsByClassName("arr")[0].style.fontSize = "20px"
-        document.getElementsByClassName("userInfo")[0].children[0].innerHTML =
-            `<img src="images/touxiang.jpg" alt="" style="width:36px; height:36px; border-radius: 50%;">`
-        document.getElementsByClassName("userInfo")[0].onmouseenter = function () {
-            document.getElementsByClassName("arr")[0].innerHTML = "&#xe506;"
-            document.getElementsByClassName("loginBox")[0].style.display = "block"
-            var str =
-                `<ul>
+if (loginOrNot) {
+    document.getElementsByClassName("arr")[0].style.position = "relative"
+    document.getElementsByClassName("arr")[0].style.top = "-12px"
+    document.getElementsByClassName("arr")[0].style.fontSize = "20px"
+    document.getElementsByClassName("userInfo")[0].children[0].innerHTML =
+        `<img src="images/touxiang.jpg" alt="" style="width:36px; height:36px; border-radius: 50%;">`
+    document.getElementsByClassName("userInfo")[0].onmouseenter = function () {
+        document.getElementsByClassName("arr")[0].innerHTML = "&#xe506;"
+        document.getElementsByClassName("loginBox")[0].style.display = "block"
+        var str =
+            `<ul>
                 <li>
                     <a href="#"><span class="iconfont">&#xe509;</span>&nbsp;&nbsp;我的订单</a>
                 </li>
@@ -28,42 +28,121 @@ loginOrNot = getCookie("login")
                     <a href="#" class="quit"><span class="iconfont">&#xe657;</span>&nbsp;&nbsp;退出</a>
                 </li>
             </ul>`
-            document.getElementsByClassName("loginBox")[0].innerHTML = str
-            $(".quit").click(function () {
-                loginOrNot = false;
-                setCookie("login", "")
-                window.location.reload()
-            })
-        }
-        document.getElementsByClassName("userInfo")[0].onmouseleave = function () {
-            document.getElementsByClassName("iconfont")[1].innerHTML = "&#xe68a;"
-            document.getElementsByClassName("loginBox")[0].style.display = "none"
-        }
+        document.getElementsByClassName("loginBox")[0].innerHTML = str
+        $(".quit").click(function () {
+            loginOrNot = false;
+            setCookie("login", "")
+            window.location.reload()
+        })
+    }
+    document.getElementsByClassName("userInfo")[0].onmouseleave = function () {
+        document.getElementsByClassName("iconfont")[1].innerHTML = "&#xe68a;"
+        document.getElementsByClassName("loginBox")[0].style.display = "none"
+    }
+} else {
+    document.getElementsByClassName("userInfo")[0].onmouseenter = function () {
+        document.getElementsByClassName("iconfont")[1].innerHTML = "&#xe506;"
+        document.getElementsByClassName("loginBox")[0].style.display = "block"
+    }
+    document.getElementsByClassName("userInfo")[0].onmouseleave = function () {
+        document.getElementsByClassName("iconfont")[1].innerHTML = "&#xe68a;"
+        document.getElementsByClassName("loginBox")[0].style.display = "none"
+    }
+}
+
+document.onscroll = function () {
+    var sTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
+    if (sTop > 100) {
+        document.getElementsByClassName("toTop")[0].style.display = "block";
     } else {
-        document.getElementsByClassName("userInfo")[0].onmouseenter = function () {
-            document.getElementsByClassName("iconfont")[1].innerHTML = "&#xe506;"
-            document.getElementsByClassName("loginBox")[0].style.display = "block"
-        }
-        document.getElementsByClassName("userInfo")[0].onmouseleave = function () {
-            document.getElementsByClassName("iconfont")[1].innerHTML = "&#xe68a;"
-            document.getElementsByClassName("loginBox")[0].style.display = "none"
-        }
+        document.getElementsByClassName("toTop")[0].style.display = "none";
     }
-    
-    document.onscroll = function () {
-        var sTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
-        if (sTop > 100) {
-            document.getElementsByClassName("toTop")[0].style.display = "block"
+    if (sTop > 800) {
+        $(".topbar").slideDown(300);
+    } else {
+        $(".topbar").slideUp(300);
+    }
+}
+$(".toTop").click(function () {
+    $("body,html").animate({
+        "scrollTop": 0
+    }, 500)
+})
+if (!loginOrNot) {
+    $(".shopcart a").attr("href", "login.html");
+    $(".rightNav a").eq(0).attr("href", "login.html");
+}
+
+function $id(id) {
+    return document.getElementById(id)
+}
+var box = document.querySelectorAll(".main-top-left")[0],
+    small = document.querySelectorAll(".imgbox")[0],
+    mask = $id("mask"),
+    big = document.querySelectorAll(".bigimg")[0],
+    bigImg = document.querySelectorAll(".bigimg img")[0],
+    smallImg = $id("smallImg");
+small.onmouseover = function () {
+    mask.style.display = "block";
+    big.style.display = "block";
+    bigImg.onmouseenter = function () {
+        mask.style.display = "none";
+        big.style.display = "none";
+    }
+}
+small.onmouseout = function () {
+    mask.style.display = "none";
+    big.style.display = "none";
+}
+small.onmousemove = function (e) {
+    var e = e || event;
+    var x = e.pageX - box.offsetLeft - mask.offsetWidth / 2;
+    var y = e.pageY - box.offsetTop - mask.offsetHeight / 2;
+    var maxL = smallImg.offsetWidth - mask.offsetWidth;
+    var maxT = smallImg.offsetHeight - mask.offsetHeight;
+    x = x < 0 ? 0 : (x > maxL ? maxL : x);
+    y = y < 0 ? 0 : (y > maxT ? maxT : y);
+    mask.style.left = x + "px";
+    mask.style.top = y + "px";
+    var bigImgLeft = x * (bigImg.offsetWidth - big.offsetWidth) / (smallImg.offsetWidth - mask.offsetWidth);
+    var bigImgTop = y * (bigImg.offsetHeight - big.offsetHeight) / (smallImg.offsetHeight - mask.offsetHeight);
+    bigImg.style.left = -bigImgLeft + "px";
+    bigImg.style.top = -bigImgTop + "px";
+}
+$(".smnav ul li").click(function () {
+    $(this).addClass("active").siblings().removeClass();
+    smallImg.src = this.children[0].src;
+    bigImg.src = this.children[0].src;
+    $(".main .main-top .main-top-right .basic .color-select ul li").eq($(this).attr("index")).addClass("active").siblings().removeClass();
+})
+$(".main .main-top .main-top-right .basic .color-select ul li").click(function () {
+    if ($(this).html() == "红色") {
+        $(this).addClass("active").siblings().removeClass();
+        $(".smnav ul li").eq(0).addClass("active").siblings().removeClass();
+        bigImg.src = smallImg.src = "images/detail/0.png";
+    } else if ($(this).html() == "黑色") {
+        $(this).addClass("active").siblings().removeClass();
+        $(".smnav ul li").eq(1).addClass("active").siblings().removeClass();
+        bigImg.src = smallImg.src = "images/detail/1.png";
+    } else {
+        $(this).addClass("active").siblings().removeClass();
+        $(".smnav ul li").eq(2).addClass("active").siblings().removeClass();
+        bigImg.src = smallImg.src = "images/detail/2.png";
+    }
+})
+$(".main .main-top .main-top-right .basic .num ul li").click(function () {
+    if ($(this).html() == "+") {
+        document.querySelectorAll(".main .main-top .main-top-right .basic .num ul input")[0].value++;
+    } else {
+        if (document.querySelectorAll(".main .main-top .main-top-right .basic .num ul input")[0].value == 1) {
+            document.querySelectorAll(".main .main-top .main-top-right .basic .num ul input")[0].value = 1;
         } else {
-            document.getElementsByClassName("toTop")[0].style.display = "none"
+            document.querySelectorAll(".main .main-top .main-top-right .basic .num ul input")[0].value--;
         }
     }
-    $(".toTop").click(function () {
-        $("body,html").animate({
-            "scrollTop": 0
-        }, 500)
-    })
-    if (!loginOrNot) {
-        $(".shopcart a").attr("href", "login.html");
-        $(".rightNav a").eq(0).attr("href", "login.html");
-    }
+})
+$(".main .taocan ul li").click(function () {
+    $(this).addClass("active").siblings().removeClass();
+    var index = $(this).attr("index")
+    document.querySelectorAll(".main .taocan img")[0].src = `images/detail/taocan/${index}.png`
+})
